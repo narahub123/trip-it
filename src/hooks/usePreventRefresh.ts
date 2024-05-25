54504a36b7ac6b224e944d536c51412f3fb099d9
nara <parknara0213@gmail.com>
2024-05-25 13:12:36 +0900

    240525 prevent refresh

---
+++ src/hooks/usePreventRefresh.ts
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+const usePreventRefresh = () => {
+  const preventClose = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = "";
+  };
+
+  useEffect(() => {
+    (() => {
+      window.addEventListener("beforeunload", preventClose);
+    })();
+
+    return () => {
+      window.removeEventListener("beforeunload", preventClose);
+    };
+  }, []);
+};
+
+export default usePreventRefresh;
--- src/layouts/ScheduleLayout.tsx
+++ src/layouts/ScheduleLayout.tsx
@@ -3,8 +3,18 @@ import "./scheduleLayout.css";
 import Procedure from "../pages/schedule/procedure/Procedure";
 import Map from "../pages/schedule/map/Map";
 import Choice from "../pages/schedule/choice/Choice";
+import { useSelector } from "react-redux";
+import { Rootstate } from "../store/store";
+import usePreventRefresh from "../hooks/usePreventRefresh";
 
 const ScheduleLayout = () => {
+  // 새로 고침 방지
+  usePreventRefresh();
+
+  // 일정 확인용(불필요)
+  const schedule = useSelector((state: Rootstate) => state.schedule);
+  console.log(schedule.schedule);
+
   return (
     <div className="scheduleLayout">
       <Procedure />
