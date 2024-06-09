dca9f69382054ed04f8e82a71432550c59bb236a
nara <parknara0213@gmail.com>
2024-06-09 20:43:43 +0900

    240609 validation: number of places and accommos

--- src/pages/schedule/procedure/Procedure.tsx
+++ src/pages/schedule/procedure/Procedure.tsx
@@ -6,6 +6,7 @@ import { clearPageNo } from "../../../store/slices/placeSlice";
 import { Rootstate } from "../../../store/store";
 import axios from "axios";
 import { CalculateDuration, dateFormatToLocalDate } from "../../../utils/date";
+import { contentTypeIds } from "../../../data/contentTypeIds";
 
 const Procedure = () => {
   const location = useLocation();
@@ -24,92 +25,39 @@ const Procedure = () => {
     (state: Rootstate) => state.columnPlaces.columnPlaces
   );
 
-  const handleSubmit = () => {
-    // 유효성 검사
-    if (schedule.metro_id === "") {
-      console.log("여행지를 선택해주세요");
-      return;
+  // 날짜 선택이 완료되지 않은 경우
+  const checkDates = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (endDate === undefined || (!endDate && endDate?.length === 0)) {
+      e.preventDefault();
+      window.alert("날짜 선택을 완료해주세요");
+      return false;
     }
 
-    if (schedule.start_date === undefined || schedule.end_date === undefined) {
-      console.log("여행 일자를 선택해주세요");
-      return;
-    }
+    return true;
+  };
 
-    if (schedule.schedule_title === "") {
-      console.log("여행의 제목을 작성해주세요");
-      return;
-    }
-
-    // if (schedule.schedule_details?.length === 0) {
-    //   console.log("여행할 장소를 선택해주세요");
-    //   return;
-    // }
-
-    // db 저장 형식에 맞춰서 date 포멧 변경
-    const start =
-      schedule.start_date && dateFormatToLocalDate(schedule.start_date);
-    const end = schedule.end_date && dateFormatToLocalDate(schedule.end_date);
-
-    const schedule_details = [];
-
-    const keys = Object.keys(columnPlaces);
-
-    for (const key of keys) {
-      const colPlaces = columnPlaces[key];
-      for (let i = 0; i < colPlaces.length; i++) {
-        const place = colPlaces[i];
-        const detail = {
-          schedule_order: key.split("columnPlaces")[1],
-          start_time: place.start_time,
-          end_time: place.end_time,
-          content_id: place.contentid,
-          createdAt: new Date().toISOString(),
-        };
-        schedule_details.push(detail);
-      }
-    }
-
-    console.log(schedule_details);
-
-    const value = {
-      metro_id: schedule.metro_id,
-      user_id: 1,
-      start_date: start,
-      end_date: end,
-      schedule_title: schedule.schedule_title,
-    };
-
-    const valueNode = {
-      metro_id: schedule.metro_id,
-      user_id: 1,
-      start_date: schedule.start_date,
-      end_date: schedule.end_date,
-      schedule_title: schedule.schedule_title,
-      schedule_details,
-    };
-
-    axios
-      .post(`http://localhost:8080/schedules`, valueNode)
-      // .post(`http://172.16.1.88:8080/home/save`, value)
-      .then((response) => {
-        if (response.status === 200 || response.status === 201) {
-          console.log("정상적으로 처리되었습니다.");
-          // navigate("/mypage");
-        }
-      })
-      .catch((error) => console.log(error.response.data));
+  const handleClickStep1 = () => {};
+  const handleClickStep2 = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    // 날짜 선택 완료 여부 확인
+    checkDates(e);
+    dispatch(clearPageNo());
+  };
+  const handleClickStep3 = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    // 날짜 선택 완료 여부 확인
+    checkDates(e);
+    dispatch(clearPageNo());
   };
 
   // step 4 유효성 검사
   const handleClickStep4 = (
     e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) => {
-    // 날짜 선택이 완료되지 않은 경우
-    if (endDate === undefined || (!endDate && endDate?.length === 0)) {
-      e.preventDefault();
-      window.alert("날짜 선택을 완료해주세요");
-    }
+    // 날짜 선택 완료 여부 확인
+    if (!checkDates(e)) return;
 
     // 장소 선택이 일정 날수보다 적은지 여부 확인
     const columnPlaces_1 = columnPlaces["columnPlaces_1"];
@@ -131,6 +79,108 @@ const Procedure = () => {
         return;
       }
     }
+  };
+
+  // 일정 제출 시 유효성 검사
+  const handleSubmit = () => {
+    // 유효성 검사
+    // 여행지 선택 여부 검사
+    if (schedule.metro_id === "") {
+      alert("여행지를 선택해주세요");
+      return;
+    }
+
+    // 여행일자 선택 여부 검사
+    if (schedule.start_date === undefined || schedule.end_date === undefined) {
+      alert("여행 일자를 선택해주세요");
+      return;
+    }
+
+    // 일정 제목 작성 여부 검사
+    if (schedule.schedule_title === "") {
+      alert("여행의 제목을 작성해주세요");
+      return;
+    }
+
+    // schedule_details에 columnPlaces 요소들 넣기
+    const schedule_details = [];
+
+    const dates = CalculateDuration(schedule.start_date, schedule.end_date).map(
+      (_, index) => index
+    );
+
+    for (const dateKey of dates) {
+      const key = `columnPlaces${dateKey}`;
+      const colPlaces = columnPlaces[key];
+      let placeCount = 0;
+      let accommoCount = 0;
+      for (let i = 0; i < colPlaces.length; i++) {
+        const place = colPlaces[i];
+        const detail = {
+          schedule_order: key.split("columnPlaces")[1],
+          start_time: place.start_time,
+          end_time: place.end_time,
+          content_id: place.contentid,
+          createdAt: new Date().toISOString(),
+        };
+        schedule_details.push(detail);
+        if (place.contenttypeid === "32") accommoCount++;
+        else placeCount++;
+      }
+      // 장소 개수 확인
+      if (placeCount < 1) {
+        alert("장소의 개수가 부족합니다");
+        return;
+      }
+      // 첫날과 마지막의 경우
+      if (dateKey === 0 || dateKey === dates.length - 1) {
+        if (accommoCount < 1) {
+          alert("숙소 개수가 부족합니다.");
+          return;
+        }
+      }
+      // 그 외의 날
+      else {
+        if (accommoCount < 2) {
+          alert("숙소 개수가 부족합니다.");
+          return;
+        }
+      }
+    }
+
+    // node js 저장시
+    const valueNode = {
+      metro_id: schedule.metro_id,
+      user_id: 1,
+      start_date: schedule.start_date,
+      end_date: schedule.end_date,
+      schedule_title: schedule.schedule_title,
+      schedule_details,
+    };
+
+    // 스프링 부트 저장시
+    // db 저장 형식에 맞춰서 date 포멧 변경
+    const start =
+      schedule.start_date && dateFormatToLocalDate(schedule.start_date);
+    const end = schedule.end_date && dateFormatToLocalDate(schedule.end_date);
+    const value = {
+      metro_id: schedule.metro_id,
+      user_id: 1,
+      start_date: start,
+      end_date: end,
+      schedule_title: schedule.schedule_title,
+    };
+
+    axios
+      .post(`http://localhost:8080/schedules`, valueNode)
+      // .post(`http://172.16.1.88:8080/home/save`, value)
+      .then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          console.log("정상적으로 처리되었습니다.");
+          navigate("/mypage");
+        }
+      })
+      .catch((error) => console.log(error.response.data));
   };
 
   return (
@@ -159,17 +209,7 @@ const Procedure = () => {
             <Link
               to="#step2"
               className={hash === "#step2" ? "link active" : "link"}
-              onClick={(e) => {
-                // 날짜 선택이 완료되지 않은 경우
-                if (
-                  endDate === undefined ||
-                  (!endDate && endDate?.length === 0)
-                ) {
-                  e.preventDefault();
-                  window.alert("날짜 선택을 완료해주세요");
-                }
-                dispatch(clearPageNo());
-              }}
+              onClick={(e) => handleClickStep2(e)}
             >
               <div className="step-container">
                 <p className="step">STEP 2 </p>
@@ -181,17 +221,7 @@ const Procedure = () => {
             <Link
               to="#step3"
               className={hash === "#step3" ? "link active" : "link"}
-              onClick={(e) => {
-                // 날짜 선택이 완료되지 않은 경우
-                if (
-                  endDate === undefined ||
-                  (!endDate && endDate?.length === 0)
-                ) {
-                  e.preventDefault();
-                  window.alert("날짜 선택을 완료해주세요");
-                }
-                dispatch(clearPageNo());
-              }}
+              onClick={(e) => handleClickStep3(e)}
             >
               <div className="step-container">
                 <p className="step">STEP 3 </p>
