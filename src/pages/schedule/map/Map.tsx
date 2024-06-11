e1f6a283f069359e22512b911a78a78d50f64f41
nara <parknara0213@gmail.com>
2024-06-11 14:14:32 +0900

    240611 multiple markers

--- src/pages/schedule/map/Map.tsx
+++ src/pages/schedule/map/Map.tsx
@@ -22,6 +22,10 @@ interface ResultType {
 }
 
 const Map = () => {
+  const selectedPlaces = useSelector(
+    (state: Rootstate) => state.place.selectedPlaces
+  );
+
   const areaCode = useSelector(
     (state: Rootstate) => state.schedule.schedule.metro_id
   );
@@ -37,7 +41,7 @@ const Map = () => {
         const container = document.getElementById("map");
         const options = {
           center: new kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
+          level: 8,
         };
 
         const map = new window.kakao.maps.Map(container, options);
@@ -46,36 +50,42 @@ const Map = () => {
         var geocoder = new window.kakao.maps.services.Geocoder();
 
         // 주소로 좌표를 검색합니다
-        geocoder.addressSearch(
-          addr,
-          function (result: ResultType[], status: kakao.maps.services.Status) {
-            // 정상적으로 검색이 완료됐으면
-            if (status === window.kakao.maps.services.Status.OK) {
-              var coords = new window.kakao.maps.LatLng(
-                result[0].y,
-                result[0].x
-              );
+        for (const selectedPlace of selectedPlaces) {
+          geocoder.addressSearch(
+            selectedPlace.addr1,
+            function (
+              result: ResultType[],
+              status: kakao.maps.services.Status
+            ) {
+              // 정상적으로 검색이 완료됐으면
+              if (status === window.kakao.maps.services.Status.OK) {
+                var coords = new window.kakao.maps.LatLng(
+                  result[0].y,
+                  result[0].x
+                );
 
-              // 결과값으로 받은 위치를 마커로 표시합니다
-              var marker = new window.kakao.maps.Marker({
-                map: map,
-                position: coords,
-              });
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new window.kakao.maps.Marker({
+                  map: map,
+                  position: coords,
+                  title: selectedPlace.title,
+                });
 
-              // 인포윈도우로 장소에 대한 설명을 표시합니다
-              var infowindow = new window.kakao.maps.InfoWindow({
-                content: `<div style="width:150px;text-align:center;padding:6px 0;">${addr}</div>`,
-              });
-              infowindow.open(map, marker);
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                var infowindow = new window.kakao.maps.InfoWindow({
+                  content: `<div style="width:150px;text-align:center;padding:6px 0;">${selectedPlace.title}</div>`,
+                });
+                infowindow.open(map, marker);
 
-              // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-              map.setCenter(coords);
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+              }
             }
-          }
-        );
+          );
+        }
       });
     }
-  }, []);
+  }, [selectedPlaces]);
   return <div id="map" style={{ width: "100%", height: "97vh" }} />;
 };
 
