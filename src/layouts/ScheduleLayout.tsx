adf90d4245bb86a06c2bcdda01b69f8a861044a7
nara <parknara0213@gmail.com>
2024-06-23 14:14:09 +0900

    240623 debounce resize

--- src/layouts/ScheduleLayout.tsx
+++ src/layouts/ScheduleLayout.tsx
@@ -13,6 +13,7 @@ import BackModal from "../pages/schedule/BackModal";
 import { setBackToggle } from "../store/slices/uiSlice";
 import { useRenderCount } from "@uidotdev/usehooks";
 import MapTest from "../pages/schedule/map/MapTest";
+import { debounce } from "../utils/debounce";
 
 const ScheduleLayout = () => {
   const scheduleLayoutRef = useRef<HTMLDivElement>(null);
@@ -32,13 +33,13 @@ const ScheduleLayout = () => {
 
   // scheduleLayout 너비 구하기: ref 이용
   useEffect(() => {
-    const handleScheduleLayoutResize = () => {
+    const handleScheduleLayoutResize = debounce(() => {
       if (!scheduleLayoutRef.current) return;
 
       const width = scheduleLayoutRef.current.getBoundingClientRect().width;
 
       setScheduleLayoutWidth(width);
-    };
+    }, 500);
 
     handleScheduleLayoutResize();
 
@@ -117,67 +118,70 @@ const ScheduleLayout = () => {
     (state: Rootstate) => state.place.accomoModal
   );
 
-  const handleResize = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!scheduleLayoutRef.current) return;
-    // choice와 map을 찾기 위해서 dom children 사용
-    const choice = scheduleLayoutRef.current.children[1] as HTMLElement;
-    const picks = choice.children[0] as HTMLElement;
-    const map = scheduleLayoutRef.current.children[3] as HTMLElement;
+  const handleResize = debounce(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      if (!scheduleLayoutRef.current) return;
+      // choice와 map을 찾기 위해서 dom children 사용
+      const choice = scheduleLayoutRef.current.children[1] as HTMLElement;
+      const picks = choice.children[0] as HTMLElement;
+      const map = scheduleLayoutRef.current.children[3] as HTMLElement;
 
-    console.log(picks);
+      console.log(picks);
 
-    // choice의 너비
-    const choiceWidth = choice.getBoundingClientRect().width;
+      // choice의 너비
+      const choiceWidth = choice.getBoundingClientRect().width;
 
-    // 최초 위치 저장
-    const initialX = e.clientX;
-    console.log("최초 위치", initialX);
+      // 최초 위치 저장
+      const initialX = e.clientX;
+      console.log("최초 위치", initialX);
 
-    const resize = (e: MouseEvent) => {
-      // 이동거리
-      const deltaX = e.clientX - initialX - 20;
+      const resize = (e: MouseEvent) => {
+        // 이동거리
+        const deltaX = e.clientX - initialX - 20;
 
-      // choice의 최소 최대 너비
-      const MIN_WIDTH = 400;
-      const MAX_WIDTH =
-        scheduleLayoutWidth < divisionPoint
-          ? divisionPoint
-          : scheduleLayoutWidth - 130; // 20차이는 choice의 padding 때문임
+        // choice의 최소 최대 너비
+        const MIN_WIDTH = 400;
+        const MAX_WIDTH =
+          scheduleLayoutWidth < divisionPoint
+            ? divisionPoint
+            : scheduleLayoutWidth - 130; // 20차이는 choice의 padding 때문임
 
-      // choice의 새로운 너비
-      let newWidth =
-        choiceWidth + deltaX < MIN_WIDTH
-          ? MIN_WIDTH
-          : choiceWidth + deltaX > MAX_WIDTH
-          ? MAX_WIDTH
-          : choiceWidth + deltaX;
+        // choice의 새로운 너비
+        let newWidth =
+          choiceWidth + deltaX < MIN_WIDTH
+            ? MIN_WIDTH
+            : choiceWidth + deltaX > MAX_WIDTH
+            ? MAX_WIDTH
+            : choiceWidth + deltaX;
 
-      console.log("이동거리", deltaX);
-      console.log("최대너비", MAX_WIDTH);
-      console.log("새로운 choice 너비", newWidth);
-      console.log("map", scheduleLayoutWidth - newWidth - 130);
+        console.log("이동거리", deltaX);
+        console.log("최대너비", MAX_WIDTH);
+        console.log("새로운 choice 너비", newWidth);
+        console.log("map", scheduleLayoutWidth - newWidth - 130);
 
-      choice.style.width = `${newWidth}px`;
-      map.style.setProperty(
-        "width",
-        `${scheduleLayoutWidth - newWidth - 130}px`
+        choice.style.width = `${newWidth}px`;
+        map.style.setProperty(
+          "width",
+          `${scheduleLayoutWidth - newWidth - 130}px`
+        );
+      };
+
+      // 마우스 이동 이벤트
+      document.addEventListener("mousemove", resize);
+
+      // 마우스 이벤트 종료
+      document.addEventListener(
+        "mouseup",
+        () => {
+          console.log("mouseup");
+
+          document.removeEventListener("mousemove", resize);
+        },
+        { once: true }
       );
-    };
-
-    // 마우스 이동 이벤트
-    document.addEventListener("mousemove", resize);
-
-    // 마우스 이벤트 종료
-    document.addEventListener(
-      "mouseup",
-      () => {
-        console.log("mouseup");
-
-        document.removeEventListener("mousemove", resize);
-      },
-      { once: true }
-    );
-  };
+    },
+    500
+  );
 
   return (
     <div className="scheduleLayout" ref={scheduleLayoutRef}>
