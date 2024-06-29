f9b094127a44a2ffba653085aeb788a68526be0b
nara <parknara0213@gmail.com>
2024-06-29 21:13:56 +0900

    240629 image change

--- src/data/metros.ts
+++ src/data/metros.ts
@@ -6,58 +6,51 @@ export const metros: MetroType[] = [
     name: "서울특별시",
     description:
       "대한민국의 수도이자 최대 도시로, 정치, 경제, 문화의 중심지입니다.",
-    imgUrl:
-      "https://res.heraldm.com/content/image/2023/08/19/20230819000100_0.jpg",
+    imgUrl: "/images/metros/metro-1.jpg",
   },
   {
     areaCode: "2",
     name: "인천광역시",
     description: "서해안에 위치한 주요 항구도시로, 인천국제공항이 있습니다.",
-    imgUrl:
-      "https://a.cdn-hotels.com/gdcs/production31/d1832/f3e68fc5-3032-4180-a2e4-ded0d70ae953.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    imgUrl: "/images/metros/metro-2.avif",
   },
   {
     areaCode: "3",
     name: "대전광역시",
     description: "과학과 교육의 도시로, 대덕연구단지가 위치해 있습니다.",
-    imgUrl:
-      "https://cdn.cctoday.co.kr/news/photo/202308/2182261_619135_653.jpg",
+    imgUrl: "/images/metros/metro-3.jpg",
   },
   {
     areaCode: "4",
     name: "대구광역시",
     description: "섬유산업과 문화예술의 중심지로 알려진 도시입니다.",
-    imgUrl:
-      "https://tour.daegu.go.kr/icms/tour/file/getImage.do?atchFileId=TOURFILE_000005472%20%20&fileSn=1",
+    imgUrl: "/images/metros/metro-4.jpg",
   },
   {
     areaCode: "5",
     name: "광주광역시",
     description: "호남지역의 중심 도시로, 문화와 예술의 도시입니다.",
-    imgUrl: "https://cdn.gjdream.com/news/photo/202202/613182_213090_365.jpg",
+    imgUrl: "/images/metros/metro-5.jpg",
   },
   {
     areaCode: "6",
     name: "부산광역시",
     description:
       "대한민국 제2의 도시로, 주요 항구도시이자 해양산업의 중심지입니다.",
-    imgUrl:
-      "https://a.cdn-hotels.com/gdcs/production37/d1169/1dcbfef5-2070-48ce-8d62-3e0fffa21797.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    imgUrl: "/images/metros/metro-6.avif",
   },
   {
     areaCode: "7",
     name: "울산광역시",
     description:
       "대한민국의 주요 공업도시로, 자동차와 조선 산업의 중심지입니다.",
-    imgUrl:
-      "https://a.cdn-hotels.com/gdcs/production55/d142/458324f3-2214-4f66-a85d-1be33dbf98ba.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    imgUrl: "/images/metros/metro-7.avif",
   },
   {
     areaCode: "8",
     name: "세종특별자치시",
     description: "행정중심복합도시로 개발된 특별자치시입니다.",
-    imgUrl:
-      "https://i.namu.wiki/i/5QhXVN4aca87EP4_DcyjFy30G-F7fE4iKKyfrwsEgx6zx_FtklOhxxdUd7Q0qVhb_lTBYsKbXD6d6m9v78Mud18UDdNXGVwd9IY_eLv6z8cQEEhHtNXzfGcjSS-xA44W0CAxU8K11DF1RHWE5bkWQw.svg",
+    imgUrl: "/images/metros/metro-8.jpg",
   },
 
   {
@@ -65,67 +58,58 @@ export const metros: MetroType[] = [
     name: "경기도",
     description:
       "서울을 둘러싸고 있는 지역으로, 대한민국에서 인구가 가장 많은 도입니다.",
-    imgUrl:
-      "https://heritage.unesco.or.kr/wp-content/uploads/2019/04/hd6_394_i1.jpg",
+    imgUrl: "/images/metros/metro-31.jpg",
   },
   {
     areaCode: "32",
     name: "강원도",
     description:
       "동해안과 태백산맥을 따라 펼쳐진 자연경관이 아름다운 지역입니다.",
-    imgUrl:
-      "https://cdn.imweb.me/upload/S201712205a3a0910b89f5/9ad67bba021e8.jpg",
+    imgUrl: "/images/metros/metro-32.jpg",
   },
   {
     areaCode: "33",
     name: "충청북도",
     description: "내륙 지방에 위치한 도로, 교육과 농업이 발달한 지역입니다.",
-    imgUrl:
-      "https://cdn.academicnews.co.kr/news/photo/202306/8311_11134_2917.jpg",
+    imgUrl: "/images/metros/metro-33.jpg",
   },
   {
     areaCode: "34",
     name: "충청남도",
     description:
       "서해안에 위치해 있으며, 산업과 농업이 균형을 이루고 있는 지역입니다.",
-    imgUrl:
-      "https://wimg.mk.co.kr/meet/neds/2021/07/image_readtop_2021_648532_16254692944706068.jpg",
+    imgUrl: "/images/metros/metro-34.jpg",
   },
   {
     areaCode: "35",
     name: "경상북도",
     description: "유서 깊은 역사와 문화를 자랑하는 지역입니다.",
-    imgUrl:
-      "https://us.123rf.com/450wm/yooranpark/yooranpark1706/yooranpark170600369/81355851-%EA%B2%BD%EC%83%81%EB%B6%81%EB%8F%84-%EA%B2%BD%EC%A3%BC-%EA%B5%AD%EB%A6%BD-%EB%B0%95%EB%AC%BC%EA%B4%80%EC%97%90%EC%9E%88%EB%8A%94-%EB%8F%8C-%ED%83%91-dabotap.jpg?ver=6",
+    imgUrl: "/images/metros/metro-35.jpg",
   },
   {
     areaCode: "36",
     name: "경상남도",
     description:
       "산업과 농업이 균형 있게 발달한 지역으로, 남해안에 위치해 있습니다.",
-    imgUrl:
-      "https://jmagazine.joins.com/_data/photo/2020/07/thumb_1971028605_tKxvDRJY_28ED86B5EC9881EC8B9C29EC868CEBA7A4EBACBCEB8F84.jpg",
+    imgUrl: "/images/metros/metro-36.jpg",
   },
   {
     areaCode: "37",
     name: "전북특별자치도",
     description: "풍부한 농산물과 전통 문화가 발달한 지역입니다.",
-    imgUrl:
-      "https://cdn.enewstoday.co.kr/news/photo/202402/2088955_890749_3232.jpg",
+    imgUrl: "/images/metros/metro-37.jpg",
   },
   {
     areaCode: "38",
     name: "전라남도",
     description: "아름다운 해안선과 다도해를 자랑하는 지역입니다.",
-    imgUrl:
-      "https://pds.joongang.co.kr//news/component/htmlphoto_mmdata/201804/27/552bb06d-5613-425e-a891-f832058c161f.jpg",
+    imgUrl: "/images/metros/metro-38.jpg",
   },
   {
     areaCode: "39",
     name: "제주특별자치도",
     description:
       "대한민국의 대표적인 관광지로, 아름다운 자연경관을 자랑합니다.",
-    imgUrl:
-      "https://a.cdn-hotels.com/gdcs/production36/d1440/d2092e2c-3944-42f1-893f-a1553738aab0.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+    imgUrl: "/images/metros/metro-39.avif",
   },
 ];
