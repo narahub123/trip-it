fb42d40acfd25804798a9a496bd0765596d1c020
nara <parknara0213@gmail.com>
2024-07-02 14:52:38 +0900

    240702 usersTableResponsive layout

--- src/pages/admin/users/UsersTableResponsive.tsx
+++ src/pages/admin/users/UsersTableResponsive.tsx
@@ -14,41 +14,10 @@ const UsersTableResponsive = ({
 }: UsersTableResponsiveProps) => {
   console.log(filteredUsers.length);
 
-  const newLimit = filteredUsers.length % 2 === 0 ? limit : limit + 1;
-
-  const modifiedUsers: UserType[] =
-    filteredUsers.length % 2 === 0
-      ? filteredUsers
-      : [
-          ...filteredUsers,
-          {
-            user_id: 0,
-            email: "hidden@gmail.com",
-            username: "hidden",
-            nickname: "hidden",
-            password: "hidden@1234",
-            birth: "hidden",
-            gender: "m",
-            user_intro: "hidden",
-            role: "hidden",
-            regdate: "hidden",
-            userpic: "",
-            report_count: 0,
-            end_date: undefined,
-          },
-        ];
-
   return (
     <table className="users-table-responsive">
-      {modifiedUsers.slice(offset, offset + newLimit).map((user) => (
-        <tr
-          className={
-            filteredUsers.length % 2 === 0
-              ? "users-table-responsive-row"
-              : "users-table-responsive-row-odd"
-          }
-          key={user.user_id}
-        >
+      {filteredUsers.slice(offset, offset + limit).map((user) => (
+        <tr className="users-table-responsive-row" key={user.user_id}>
           <td>
             <span className="cell-header">아이디</span> {user.user_id}
           </td>
