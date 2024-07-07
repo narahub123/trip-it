c045ff466bc0520564f163bb805b8ad9ce7c71c3
nara <parknara0213@gmail.com>
2024-07-08 02:05:26 +0900

    240708 mypage authentication

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const Admin = () => {
   return (
     <div className="admin" data-theme="light">
-      {/* <DarkMode /> */}
+      <DarkMode />
       <Outlet />
     </div>
   );
--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import "./mypage.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const MyPage = () => {
+  const navigate = useNavigate();
+  const baseURL = process.env.REACT_APP_SERVER_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/mypage`, { withCredentials: true })
+      .then()
+      .catch((err) => {
+        if (err.response.data.code === "logout") {
+          window.alert("로그인을 해주세요");
+          navigate("/login");
+        }
+      });
+  }, []);
   return (
     <div className="mypage">
       <Outlet />
